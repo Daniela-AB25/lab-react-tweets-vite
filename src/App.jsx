b953f5d92394dea5276cb9a59aff50557a4415d4
Daniela Astorga Bilbao <25daniela99ab@gmail.com>
2024-04-30 19:23:21 +0200

    Lab done

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const tweetsArray = [
     message:
       "the human likes to say. that i live here rent free. but i would argue. this housing accommodation. is my payment. for a lifetime of love. and excellent company",
   },
+
   {
     user: {
       name: "Thoughts of Dog®",
@@ -24,6 +25,7 @@ const tweetsArray = [
     message:
       "sometimes. the human presses their noggin against mine. to figure out what i’m thinking. so i just think really hard. about how much i love them. and hope they figure it out",
   },
+
   {
     user: {
       name: "Thoughts of Dog®",
@@ -38,9 +40,16 @@ const tweetsArray = [
 ];
 
 function App() {
+
   return (
     <div className="App">
-      <Tweet />
+
+      <Tweet tweet={tweetsArray[0]} />
+      <Tweet tweet={tweetsArray[1]} />
+      <Tweet tweet={tweetsArray[2]} />
+
+
+
     </div>
   );
 }
